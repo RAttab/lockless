127af3be97060d04015bb3362e40b8fbfa2d3e1f
Rémi Attab <remi.attab@gmail.com>
2012-12-20 20:59:11 -0500

    Map: Almost working implementation of insertImpl.

--- map.h
+++ map.h
@@ -15,6 +15,7 @@
 #include <atomic>
 #include <cassert>
 #include <cstdlib>
+#include <type_traits>
 
 namespace lockless {
 
@@ -36,13 +37,27 @@ namespace lockless {
 template<
     typename Key,
     typename Value,
-    typename Hash = std::hash<K>
+    typename Hash = std::hash<K>,
     typename MKey = MagicValue<Key>,
     typename MValue = MagicValue<Value> >
 struct Map
 {
 
 private:
+
+    enum Policy
+    {
+        ResizeThreshold = 4,
+        CleanupThreshold = 4
+    };
+
+    enum InsertResult
+    {
+        None = 0,
+        KeyExists,
+        KeyInserted,
+        TryAgain,
+    };
 
     typedef details::Atomizer<Key> KeyAtomizer;
     typedef KeyAtomizer::type KeyAtom;
@@ -139,13 +154,44 @@ private:
         return newest;
     }
 
+
+    bool isKeyMarked(KeyAtom atom) const
+    {
+        return isEmpty(atom) || isTomstone(atom);
+    }
+
+    bool isKeyEmpty(KeyAtom atom) const
+    {
+        return atom & MKey::mask0;
+    }
+
+    bool isKeyTombstone(KeyAtom atom) const
+    {
+        return atom & MKey::mask1;
+    }
+
+    bool isValueMarked(ValueAtom atom) const
+    {
+        return isEmpty(atom) || isTomstone(atom);
+    }
+
+    bool isValueEmpty(ValueAtom atom) const
+    {
+        return atom & MKey::mask0;
+    }
+
+    bool isValueTombstone(ValueAtom atom) const
+    {
+        return atom & MKey::mask1;
+    }
+
     Table* moveBucket(Table* dest, Bucket& src)
     {
         // tombstone the bucket so nothing can be inserted there.
         KeyAtom keyAtom = bucket.exchange(MKey::mask1);
-        ValueAtom valueAtom = bucket.load();
+        ValueAtom valueAtom = bucket.exchange(MValue::mask1);
 
-        if (isMagicValue(keyAtom) || isMagicValue(valueAtom))
+        if (isKeyMarked(keyAtom) || isValueMarked(valueAtom))
             return;
 
         Key key = KeyAtomizer::load(keyAtom);
@@ -153,7 +199,7 @@ private:
 
         // The insert may fail if there's a move going into the dest table. In
         // this case load the dest's next table and try to move into that table.
-        while(!insert(dest, hash, keyAtom, valueAtom)) {
+        while(!insert(dest, hash, key, keyAtom, valueAtom)) {
             Table* next = dest->next.load();
 
             // Resizing during a resize... What could possibly go wrong?
@@ -214,8 +260,115 @@ private:
         return newTable;
     }
 
-    bool insert(Table* t, size_t hash, KeyAtom keyAtom, ValueAtom valueAtom)
+    size_t bucket(size_t hash, size_t i, size_t capacity)
     {
+        // Capacity is an exponent of 2 so this will work just fine.
+        return (hash + i) & (capacity - 1);
+    }
+
+    void checkPolicies(size_t capacity, size_t probes, size_t tombstones)
+    {
+        if (probes >= RESIZE_THRESHOLD)
+            resizeImpl(capacity * 2);
+
+#if 0 // Could trigger a double resizize. Disable for now.
+        if (tombstones >= TOMBSTONE_THRESHOLD)
+            resizeImpl(capacity, true);
+#endif
+    }
+
+    /*
+
+       Horribly complicated explanation of why this won't race with moveBucket:
+
+       So moveBucket moves items by tombstombing both K & V then moves them to
+       the new table. Now if an insert or find or whatever for that key shows up
+       in between the move then it has to go through the table being moved.
+
+       This leads to the following scenarios:
+
+       - The op is there before the tombstoning. At which point it wins and the
+         move op moves the value. Nothing too complicated there.
+
+       - The op is there during the tombstoning and wins. See above.
+
+       - The op is there during the tombstoning and losses. At which point it
+         has to try again on the same table. This turns out to be the same as
+         the scenario below.
+
+         \todo Blargh bug...
+     */
+    InsertResult
+    insertImpl(
+            Table* t, size_t hash,
+            Key key, KeyAtom keyAtom,
+            ValueAtom valueAtom)
+    {
+        InsertResult result = None;
+        size_t i;
+        size_t tombstones = 0;
+
+        for (i = 0; i < t->capacity; ++i) {
+            Bucket& bucket = t->buckets[this->bucket(hash, i, t->capacity)];
+
+            KeyAtom bucketKeyAtom = bucket.key.load();
+            if (isKeyTomstone(bucketKeyAtom)) {
+                tombstones++;
+                continue;
+            }
+
+            if (!isKeyEmpty(bucketKeyAtom)) {
+                Key bucketKey = KeyAtomizer::load(bucketKeyAtom);
+
+                if (bucketKey != key) {
+                    // \todo if t->isResizing then move(newest(), bucket);
+                    continue;
+                }
+            }
+
+            // If the key in the bucket is a match we can skip this step. The
+            // idea is if we have 2+ concurrent inserts for the same key then
+            // the winner is determined when writing the value.
+            else if (!bucket.key.compare_exchange_weak(bucketKeyAtom, keyAtom)) {
+                // Someone beat us; try again the same bucket again.
+                --i;
+                continue;
+            }
+
+
+            ValueAtom bucketValueAtom = bucket.value.load();
+
+            size_t dbg_loops = 0;
+
+            // This loop can only start over once. The first pass is to avoid a
+            // pointless cas (an optimization). The second go around is to redo
+            // the same checks after we fail. This means that the cas is only
+            // executed once.
+            do {
+                // Another insert beat us to it.
+                if (!isValueEmpty(bucketValueAtom)) {
+                    result = KeyExists;
+                    break;
+                }
+
+                // Beaten by a move operation. Since we're interfering with a
+                // move, no point to keep on trying.
+                if (isValueTombstone(bucketValueAtom)) {
+                    result = TryAgain;
+                    break;
+                }
+
+                assert(!dbg_loops);
+                dbg_loops++;
+
+            } while (!bucket.key.compare_exchange_strong(bucketValueAtom, valueAtom));
+
+            if (!result) result = KeyInserted;
+            break;
+        }
+
+        checkPolicies(t->capacity, i, tombstones);
+        return result;
     }
 
     Hash hashFn;
