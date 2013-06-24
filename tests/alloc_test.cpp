9be9a3fae1ea54037bd7b8c7ec507dc5068b755a
Rémi Attab <remi.attab@gmail.com>
2013-06-23 20:40:43 -0400

    Alloc: Changed the the test to use boost preproc.

--- tests/alloc_test.cpp
+++ tests/alloc_test.cpp
@@ -14,11 +14,23 @@
 #include "test_utils.h"
 
 #include <boost/test/unit_test.hpp>
+#include <boost/preprocessor/seq/for_each.hpp>
 
 
 using namespace std;
 using namespace lockless;
 
+
+#define SIZE_SEQ                                                       \
+    (0x00) (0x01) (0x02) (0x03) (0x04) (0x05) (0x06) (0x07) (0x08) (0x0C) \
+    (0x10) (0x11) (0x18) (0x20) (0x30) (0x40) (0x80)                    \
+    (0x0100) (0x0111) (0x0200) (0x0400) (0x0800) (0x1000) (0x1111)      \
+    (0x0000010000) (0x0000100000) (0x0001000000) (0x0001111111)
+
+
+/******************************************************************************/
+/* POLICY TEST                                                                */
+/******************************************************************************/
 
 void checkPow2(size_t val)
 {
@@ -42,37 +54,11 @@ void checkPolicy(const std::function<void (size_t, size_t)>& fn)
     fn(Policy::BlockSize, Policy::PageSize);
 }
 
-#define PolicyTest(Policy, checkFn)                     \
-    do {                                                \
-        checkPolicy< Policy<0x00> >(checkFn);           \
-        checkPolicy< Policy<0x01> >(checkFn);           \
-        checkPolicy< Policy<0x02> >(checkFn);           \
-        checkPolicy< Policy<0x03> >(checkFn);           \
-        checkPolicy< Policy<0x04> >(checkFn);           \
-        checkPolicy< Policy<0x05> >(checkFn);           \
-        checkPolicy< Policy<0x06> >(checkFn);           \
-        checkPolicy< Policy<0x07> >(checkFn);           \
-        checkPolicy< Policy<0x08> >(checkFn);           \
-        checkPolicy< Policy<0x0C> >(checkFn);           \
-        checkPolicy< Policy<0x10> >(checkFn);           \
-        checkPolicy< Policy<0x18> >(checkFn);           \
-        checkPolicy< Policy<0x20> >(checkFn);           \
-        checkPolicy< Policy<0x30> >(checkFn);           \
-        checkPolicy< Policy<0x40> >(checkFn);           \
-        checkPolicy< Policy<0x80> >(checkFn);           \
-        checkPolicy< Policy<0x0100> >(checkFn);         \
-        checkPolicy< Policy<0x0200> >(checkFn);         \
-        checkPolicy< Policy<0x0400> >(checkFn);         \
-        checkPolicy< Policy<0x0800> >(checkFn);         \
-        checkPolicy< Policy<0x1000> >(checkFn);         \
-        checkPolicy< Policy<0x0000010000> >(checkFn);   \
-        checkPolicy< Policy<0x0000100000> >(checkFn);   \
-        checkPolicy< Policy<0x0001000000> >(checkFn);   \
-    } while(false);
-
-
 BOOST_AUTO_TEST_CASE(policyTest)
 {
+
+    cerr << fmtTitle("PackedAllocPolicy", '=') << endl;
+
     auto checkFn = [] (size_t blockSize, size_t pageSize) {
         checkPow2(pageSize);
         locklessCheckGe(pageSize, 4096ULL, NullLog);
@@ -81,17 +67,34 @@ BOOST_AUTO_TEST_CASE(policyTest)
             locklessCheckGt(pageSize / blockSize, 64ULL, NullLog);
     };
 
+#define PackedAllocPolicyTest(_r_, _data_, _elem_)      \
+    checkPolicy< PackedAllocPolicy<_elem_> >(checkFn);
+
+    BOOST_PP_SEQ_FOR_EACH(PackedAllocPolicyTest, _, SIZE_SEQ);
+
+#undef PackedAllocPolicyTest
+
+
+    cerr << fmtTitle("AlignedAllocPolicy", '=') << endl;
+
     auto checkAlignFn = [&] (size_t blockSize, size_t pageSize) {
         checkFn(blockSize, pageSize);
         checkAlign(blockSize, 8);
     };
 
-    cerr << fmtTitle("PackedAllocPolicy", '=') << endl;
-    PolicyTest(PackedAllocPolicy, checkFn);
 
-    cerr << fmtTitle("AlignedAllocPolicy", '=') << endl;
-    PolicyTest(AlignedAllocPolicy, checkAlignFn);
+#define AlignedAllocPolicyTest(_r_, _data_, _elem_)             \
+    checkPolicy< AlignedAllocPolicy<_elem_> >(checkAlignFn);
+
+    BOOST_PP_SEQ_FOR_EACH(AlignedAllocPolicyTest, _, SIZE_SEQ);
+
+#undef PackedAllocPolicyTest
 }
+
+
+/******************************************************************************/
+/* PAGE TEST                                                                  */
+/******************************************************************************/
 
 BOOST_AUTO_TEST_CASE(pageTest)
 {
